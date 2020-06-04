a1cb3115b9be7b300ec86d0cd53033ab161df46a
Kyomotoi <1172294279@qq.com>
2020-06-04 12:57:23 +0800

    Added some functions(hitokoto weather)

--- AyaBot/plugins/hitokoto.py
+++ AyaBot/plugins/hitokoto.py
@@ -1,30 +1,30 @@
 import json
+import asyncio
 import requests
-import pandas as pd
+from typing import Any
+from functools import partial
 from nonebot import on_command, CommandSession
-from datetime import datetime
 
 
-url_1 = 'https://api.imjad.cn/hitokoto/?cat=a&charset=utf-8&length=50&encode=json&fun=sync&source='
-response_1 = requests.get(url=url_1).json()
-data_1 = json.load(response_1)
+class AsyncResponse:
+    def __init__(self, response: requests.Response):
+        self.raw_response = response
 
-filename1 = "data_1.json"
+async def run_sync_func(func, *args, **kwargs) -> Any:
+    return await asyncio.get_event_loop().run_in_executor(
+        None, partial(func, *args, **kwargs))
 
-LIST = """一言
-{hitokoto}
-by {source}
-"""
+async def request(method, url, **kwargs) -> AsyncResponse:
+    return AsyncResponse(await run_sync_func(requests.request,
+                                             method=method, url=url, **kwargs))
+
+
+url = "https://api.imjad.cn/hitokoto/?cat=a&charset=utf-8&length=50&encode=&fun=sync&source="
+
 
 @on_command('hitokoto', aliases=['一言'], only_to_me=False)
 async def _(session: CommandSession):
-    f = open(filename1, encoding='utf-8')
-    setting = json.load(f)
-    await session.send(LIST.format(
-        hitokoto=setting["hitokoto"],
-        source=setting["source"]
-        )
-    )
-
-
-#开发ing
+    res = requests.get(url)
+    if not res.ok:
+        session.finish('获取失败')
+    session.finish(res.text)
--- AyaBot/plugins/weather.py
+++ AyaBot/plugins/weather.py
@@ -1,22 +1,46 @@
-import re
+import json
+import requests
 from nonebot import on_command, CommandSession
 
 
-@on_command('weather', aliases=['天气', '查天气', '天气查询'])
+API_URL = 'https://www.tianqiapi.com/free/day?appid=36628957&appsecret=WKn4dtVg&city='
+
+
+LIST = """{city} 今日信息如下:
+更新时间:{time}
+天气情况:{wea}
+空气质量:{air}
+温度:
+    现在温度:{tem}
+    最高温度:{temday}
+    最低温度:{temnight}
+风向:{win}
+风力等级:{winspeed}
+风速:{winmeter}"""
+
+
+@on_command('weather', aliases=['天气', '查天气', '天气查询'], only_to_me=False)
 async def weather(session: CommandSession):
     city = session.get('city', prompt='你想查哪个城市呢？')
-    date = session.get('date', prompt='你想查哪一天呢？(格式:20200427)')
-    await session.send('你查询的情况如下' + city)
-    await session.send('你想查询的日期' + date)
-
-
-@weather.args_parser
-async def _(session: CommandSession):
-    if session.is_first_run:
+    try:
+        res = API_URL + city
+        res1 = requests.get(res)
+        res1.encoding = 'utf-8'
+        html = res1.text
+        wt = json.loads(html)
+        await session.send(LIST.format(
+            city=wt["city"],
+            time=wt["update_time"],
+            wea=wt["wea"],
+            tem=wt["tem"],
+            temday=wt["tem_day"],
+            temnight=wt["tem_night"],
+            win=wt["win"],
+            winspeed=wt["win_speed"],
+            winmeter=wt["win_meter"],
+            air=wt["air"]
+            )
+        )
+    except:
+        await session.send('获取数据时出问题，请重试')
         return
-
-    if session.current_key == 'date':
-        if not re.fullmatch(r'\d{8}', session.current_arg_text):
-            session.pause('日期格式有误，请重新输入')
-    session.args[session.current_key] = session.current_arg_text
-
