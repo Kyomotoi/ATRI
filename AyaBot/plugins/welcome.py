d342a5ab934db769593892267c4ab223d2a77b60
Kyomotoi <1172294279@qq.com>
2020-06-24 21:57:12 +0800

    [FIX] welcome

--- AyaBot/plugins/welcome.py
+++ AyaBot/plugins/welcome.py
@@ -10,8 +10,10 @@ master = bot.config.SUPERUSERS
 
 @on_notice('group_increase')
 async def increase(session: NoticeSession):
-    q = session.ctx['user_id']
-    me = session.ctx['self_id']
+    # q = session.ctx['user_id']
+    # me = session.ctx['self_id']
+    q = session.event.user_id
+    me = session.event.self_id
     if q==me:
         await session.send('在下綾，今后请多指教呐')
     else:
@@ -19,8 +21,8 @@ async def increase(session: NoticeSession):
 
 @on_notice('group_decrease')
 async def decrease(session: NoticeSession):
-    q = str(session.ctx['user_id'])
-    m = str(session.ctx['operator_id'])
+    q = str(session.event.user_id)
+    m = str(session.event.operator_id)
     if m==q:
         inf=await bot.get_stranger_info(user_id=q)
         name=inf['nickname']
@@ -29,9 +31,9 @@ async def decrease(session: NoticeSession):
 @on_request('group')
 async def bot_request(session: RequestSession):
     global request_flag
-    f_group= session.ctx['group_id']
-    f_user= session.ctx['user_id']
-    if session.ctx['sub_type']=='invite':
+    f_group= session.event.group_id
+    f_user= session.event.user_id
+    if session.event.sub_type=='invite':
         if request_flag == False:
             await session.bot.send_private_msg(user_id=f_user,message=f'想邀请綾入群嘛？请先点击这里联系作者：\nhttps://lolihub.icu/#/about')
             await session.bot.send_private_msg(user_id=master,message=f'有新的群邀请:\n群：{f_group}\n邀请人：{f_user}\n已忽略')
@@ -45,12 +47,12 @@ async def bot_request(session: RequestSession):
 
 @on_notice('friend_add')
 async def friend_add(session: NoticeSession):
-    f_user=session.ctx['user_id']
+    f_user=session.event.user_id
     await bot.send_private_msg(user_id=master,message=f'有人加我啦！\n{f_user}')
 
 @on_command('set_request', aliases=('开启邀请',), only_to_me=True)
 async def set_request(session: CommandSession):
     global request_flag
-    if session.ctx['user_id']==master:
+    if session.event.user_id==master:
         request_flag=True
         await session.bot.send_private_msg(user_id=master,message='已开启')
