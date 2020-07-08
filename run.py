1b62dfb2f80fe09aa977b7f7d1e933e454c8b599
Kyomotoi <1172294279@qq.com>
2020-07-08 09:28:30 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,32 +1,22 @@
 # -*- coding:utf-8 -*-
 import time
 COPYRIGHT = (
-    r"""
-                       _____            _  ____  ____        _   
-     /\               / ____|          | |/ __ \|  _ \      | |  
-    /  \  _   _  __ _| |     ___   ___ | | |  | | |_) | ___ | |_ 
-   / /\ \| | | |/ _` | |    / _ \ / _ \| | |  | |  _ < / _ \| __|
-  / ____ \ |_| | (_| | |___| (_) | (_) | | |__| | |_) | (_) | |_ 
- /_/    \_\__, |\__,_|\_____\___/ \___/|_|\___\_\____/ \___/ \__|
-           __/ |                                                 
-          |___/                                                  
-
+    r"""AyaCoolQBot
 Copyright © 2018-2020 Kyomotoi,All Rights Reserved
 Project: https://github.com/Kyomotoi/Aya
 Blog: lolihub.icu
-===================================================
-"""
+==================================================="""
 )
 print(COPYRIGHT)
-time.sleep(3)
+time.sleep(0)
 
 
 from AyaBot import config
-time.sleep(3)
+time.sleep(0)
 print("开始执行主程序...RCnb!")
 
 
-time.sleep(2)
+time.sleep(0)
 from os import path
 from AyaBot.plugins import module
 
