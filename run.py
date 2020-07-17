77514e99f0308ed90dc031810f71c67771971878
Kyomotoi <1172294279@qq.com>
2020-07-17 21:36:55 +0800

    [FIX]

--- run.py
+++ run.py
@@ -1,24 +1,24 @@
 # -*- coding:utf-8 -*-
 import time
 COPYRIGHT = (
-    r"""ATRI bot byPython
+    r"""ATRI | アトリ
 Copyright © 2018-2020 Kyomotoi,All Rights Reserved
 Project: https://github.com/Kyomotoi/Aya
 Blog: lolihub.icu
 ==================================================="""
 )
 print(COPYRIGHT)
-time.sleep(2)
+time.sleep(0)
 
 
-from AyaBot import config
-time.sleep(2)
+from ATRIbot import config
+time.sleep(0)
 print("开始执行主程序...RCnb!")
 
 
-time.sleep(2)
+time.sleep(0)
 from os import path
-from AyaBot.plugins import module
+from ATRIbot.plugins import module
 
 
 if __name__ == '__main__':
@@ -26,6 +26,6 @@ if __name__ == '__main__':
     nonebot.init(config)
     nonebot.load_builtin_plugins()
     nonebot.load_plugins(
-        path.join(path.dirname(__file__), 'AyaBot', 'plugins'),
-        'AyaBot.plugins')
+        path.join(path.dirname(__file__), 'ATRIbot', 'plugins'),
+        'ATRIbot.plugins')
     nonebot.run()
