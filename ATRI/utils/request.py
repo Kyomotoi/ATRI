c2e754b5456c454ed6bb08a9a525a40ef16e6ec4
Kyomotoi <Kyomotoiowo@gmail.com>
2021-10-01 13:39:46 +0800

    🚑️ 紧急删除

--- ATRI/utils/request.py
+++ ATRI/utils/request.py
@@ -2,10 +2,10 @@ import httpx
 
 
 async def get(url: str, **kwargs):
-    async with httpx.AsyncClient(proxies=proxy) as client:
+    async with httpx.AsyncClient() as client:
         return await client.get(url, **kwargs)
 
 
 async def post(url: str, **kwargs):
-    async with httpx.AsyncClient(proxies=proxy) as client:
+    async with httpx.AsyncClient() as client:
         return await client.post(url, **kwargs)
