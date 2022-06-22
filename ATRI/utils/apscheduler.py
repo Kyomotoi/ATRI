8ef3dcab8efc7aa14c3ade2f74bacd2832d29e0b
Kyomotoi <kyomotoiowo@gmail.com>
2022-06-22 19:30:14 +0800

    cd /Volumes/0w0/ATRI ; /usr/bin/env /Users/kyomotoi/Library/Caches/pypoetry/virtualenvs/atri-ffQpDJDv-py3.8/bin/python /Users/kyomotoi/.vscode/extensions/ms-python.python-2022.9.11681004/pythonFiles/lib/python/debugpy/launcher 62387 -- /Volumes/0w0/ATRI/az.py

--- ATRI/utils/apscheduler.py
+++ ATRI/utils/apscheduler.py
@@ -8,6 +8,6 @@ scheduler = AsyncIOScheduler(timezone="Asia/Shanghai")
 
 
 aps_logger = logging.getLogger("apscheduler")
-aps_logger.setLevel(logging.DEBUG)
+aps_logger.setLevel(30)
 aps_logger.handlers.clear()
 aps_logger.addHandler(LoguruHandler())
