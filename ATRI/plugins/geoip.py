009c3e9242038d6880a80b65378e4b2051d12530
Lint Action <noreply@github.com>
2022-11-12 11:26:37 +0000

    :rotating_light: 自动进行代码格式化

--- ATRI/plugins/geoip.py
+++ ATRI/plugins/geoip.py
@@ -13,16 +13,13 @@ LANG = "zh-CN"
 
 @query_geoip.got("ip_address", prompt="地址是?(支持ipv4/ipv6)")
 async def _(ip_address: str = ArgStr()):
-    with geoip2.webservice.Client(conf.GeoIP.account_id,
-                                  conf.GeoIP.license_key,
-                                  host="geolite.info") as client:
+    with geoip2.webservice.Client(
+        conf.GeoIP.account_id, conf.GeoIP.license_key, host="geolite.info"
+    ) as client:
         response = client.city(ip_address)
         country = response.country.names[LANG]
         city = response.city.names[LANG]
         subdivision = ""
         if response.subdivisions:
             subdivision = response.subdivisions[0].names[LANG]
-        await query_geoip.finish(
-            f"IP: {ip_address}\n"
-            f"{country}{subdivision}{city}"
-        )
+        await query_geoip.finish(f"IP: {ip_address}\n" f"{country}{subdivision}{city}")
