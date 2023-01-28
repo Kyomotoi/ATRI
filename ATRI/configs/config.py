88405472802c82d39f1826d438d607f5b91a6011
Kyomotoi <0w0@imki.moe>
2023-01-28 22:41:43 +0800

    🎨 优化输出

--- ATRI/configs/config.py
+++ ATRI/configs/config.py
@@ -5,8 +5,8 @@ from pathlib import Path
 from .models import BotConfig, ConfigModel, WithGoCQHTTP, RuntimeConfig
 
 
-CONFIG_PATH = Path(".") / "data" / "config"
-CONFIG_PATH.mkdir(parents=True, exist_ok=True)
+CONFIG_DATA_PATH = Path(".") / "data" / "config"
+CONFIG_DATA_PATH.mkdir(parents=True, exist_ok=True)
 
 _DEFAULT_CONFIG_PATH = Path(".") / "ATRI" / "configs" / "default_config.yml"
 
@@ -18,18 +18,18 @@ class Config:
                 with open(config_path, "w", encoding="utf-8") as w:
                     w.write(_DEFAULT_CONFIG_PATH.read_text("utf-8"))
 
-                print("!!! 未检测到 config.yml, 已于当前目录生成, 请参照文档进行填写并重新启动")
-                print("!!! 文档地址: https://atri.imki.moe/install/configuration-bot/")
+                print("[!] 未检测到 config.yml, 已于当前目录生成, 请参照文档进行填写并重新启动")
+                print("[!] 文档地址: https://atri.imki.moe/install/configuration-bot/")
                 sleep(3)
                 exit(0)
             except Exception as err:
-                print(f"!!! 写入文件 config.yml 失败, 请确认是否给足权限: {err}")
+                print(f"[x] 写入文件 config.yml 失败, 请确认是否给足权限: {err}")
                 sleep(3)
                 exit(-1)
         else:
             with open(config_path, "r", encoding="utf-8") as r:
                 if "BotSelfConfig" in r.read():
-                    print("!!! 你的 config.yml 文件已废弃, 请 删除/备份 并重新启动")
+                    print("[!] 你的 config.yml 文件已废弃, 请 删除/备份 并重新启动")
                     sleep(3)
                     exit(-1)
 
